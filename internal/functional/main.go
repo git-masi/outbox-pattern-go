e9b1c747a5a9a612e2d958b17ba5d8085d23edf4
git-masi <eric.masi.dev@gmail.com>
2022-12-03 10:32:57 -0500

    Add filter function

---
+++ internal/functional/main.go
@@ -0,0 +1,16 @@
+// These functions come from:
+//		https://bitfieldconsulting.com/golang/functional
+
+package functional
+
+type keepFunc[E any] func(E) bool
+
+func Filter[E any](s []E, f keepFunc[E]) []E {
+	result := []E{}
+	for _, v := range s {
+		if f(v) {
+			result = append(result, v)
+		}
+	}
+	return result
+}
